0466ea6e6574932023643a8d9d811659a85f8549
lazarv <lazarv1982@gmail.com>
2023-09-28 23:08:33 +0200

    fix: router imports
    Fixes file types for ActionState and Params. Updates import of these files.
    
    This fixes an issue with production build when used the framework from node_modules
    instead of this monorepo or as a linked dependency as a symlink.

--- packages/react-server/server/router.jsx
+++ packages/react-server/server/router.jsx
@@ -1,6 +1,6 @@
-import { useActionState } from "@lazarv/react-server/client/ActionState.mjs";
+import { useActionState } from "@lazarv/react-server/client/ActionState.jsx";
 import ClientOnly from "@lazarv/react-server/client/ClientOnly.jsx";
-import { useParams } from "@lazarv/react-server/client/Params.mjs";
+import { useParams } from "@lazarv/react-server/client/Params.jsx";
 
 import Route, { useMatch } from "./Route.jsx";
 export { Route, ClientOnly, useMatch, useParams, useActionState };

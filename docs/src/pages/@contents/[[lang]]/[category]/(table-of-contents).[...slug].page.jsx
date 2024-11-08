a27f9161cd16e5c6dc8e7b50b4393e12724663cc
Viktor Lázár <lazarv1982@gmail.com>
2024-11-08 21:30:36 +0100

    fix(docs): table of contents rendering (#73)
    This PR fixes the rendering of the table of contents in the docs by
    using a frontmatter flag to determine during SSR whether a contents
    sidebar should be rendered.

--- docs/src/pages/@contents/[[lang]]/[category]/(table-of-contents).[...slug].page.jsx
+++ docs/src/pages/@contents/[[lang]]/[category]/(table-of-contents).[...slug].page.jsx
@@ -1,7 +1,17 @@
+import { usePathname } from "@lazarv/react-server";
+
 import Sidebar from "../../../../components/Sidebar.jsx";
 import TableOfContents from "../../../../components/TableOfContents.jsx";
+import { getPages } from "../../../../pages.mjs";
 
-export default function Contents() {
+export default function Contents({ lang, category }) {
+  const pathname = usePathname();
+  const { frontmatter } = getPages(pathname, lang)
+    .find(({ category: c }) => c.toLowerCase() === category.toLowerCase())
+    ?.pages.find(({ langHref }) => langHref === pathname);
+
+  if (frontmatter?.contents === false) return null;
+
   return (
     <Sidebar id="contents" menu="On this page" right>
       <TableOfContents />

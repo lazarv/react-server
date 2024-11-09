9f3b6b9ae6681c6dff2fb0f26d648ca321dab713
Viktor Lázár <lazarv1982@gmail.com>
2024-11-09 22:05:07 +0100

    fix(docs): not found page (#75)
    Fixes styling and page components on the docs "Not Found" page

--- docs/src/pages/@contents/[[lang]]/[category]/(table-of-contents).[...slug].page.jsx
+++ docs/src/pages/@contents/[[lang]]/[category]/(table-of-contents).[...slug].page.jsx
@@ -6,11 +6,12 @@ import { getPages } from "../../../../pages.mjs";
 
 export default function Contents({ lang, category }) {
   const pathname = usePathname();
-  const { frontmatter } = getPages(pathname, lang)
-    .find(({ category: c }) => c.toLowerCase() === category.toLowerCase())
-    ?.pages.find(({ langHref }) => langHref === pathname);
+  const { frontmatter } =
+    getPages(pathname, lang)
+      .find(({ category: c }) => c.toLowerCase() === category.toLowerCase())
+      ?.pages.find(({ langHref }) => langHref === pathname) ?? {};
 
-  if (frontmatter?.contents === false) return null;
+  if (!frontmatter || frontmatter?.contents === false) return null;
 
   return (
     <Sidebar id="contents" menu="On this page" right>

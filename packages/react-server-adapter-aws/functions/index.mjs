8eebed1bc3554d7f0e4a3ad2c9236badbe635944
Andreas Heissenberger <andreas@heissenberger.at>
2024-12-17 11:26:49 +0100

    fix: linting

--- packages/react-server-adapter-aws/functions/index.mjs
+++ packages/react-server-adapter-aws/functions/index.mjs
@@ -1,9 +1,9 @@
 import { createHandler } from "@lazarv/react-server-adapter-aws/create-handler";
 import { awsLambdaAdapter as lambdaHandler } from "@lazarv/react-server-adapter-aws/hono-lambda-adapter";
 
-export const handler = lambdaHandler(
-  await createHandler({
-    origin: process.env.ORIGIN || "http://localhost:3000",
-    outDir: process.env?.OUT_DIR,
-  })
-);
+const rsHandler = await createHandler({
+  origin: process.env.ORIGIN || "http://localhost:3000",
+  outDir: process.env?.OUT_DIR,
+});
+
+export const handler = lambdaHandler(rsHandler);

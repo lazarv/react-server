6c0eca065fcc91ac6fe44a08dc771bf8f2de43cd
Viktor Lázár <lazarv1982@gmail.com>
2025-02-09 12:26:20 +0100

    fix: builtin import test on win32

--- test/fixtures/builtin-import.jsx
+++ test/fixtures/builtin-import.jsx
@@ -3,5 +3,5 @@ import { createRequire } from "module";
 const _require = createRequire(import.meta.url);
 
 export default function Builtin() {
-  return <>{_require.resolve("react")}</>;
+  return <>{_require.resolve("react").replace(/\\/g, "/")}</>;
 }

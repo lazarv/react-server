f69ff631255a7a7bfd8d38027ba7f45f485720cb
Viktor Lázár <lazarv1982@gmail.com>
2025-02-27 23:49:49 +0100

    feat: tailwindcss v4

--- packages/create-react-server/templates/vite.config.tailwind-v4.ts
+++ packages/create-react-server/templates/vite.config.tailwind-v4.ts
@@ -2,10 +2,5 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@/": new URL("src/", import.meta.url).pathname,
-    },
-  },
   plugins: [tailwindcss()],
 });

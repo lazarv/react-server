ed80235453848f4659e93c7024c239e4df2529f0
Viktor Lázár <lazarv1982@gmail.com>
2025-03-25 19:42:09 +0100

    feat: upgrade dependencies (#190)
    Upgrades dependencies
    
    - React 0.0.0-experimental-740a4f7a-20250325
    - Vite 6.2.3
    - HatTip 0.0.49
    
    and other minor upgrades

--- examples/remote/react-server.production.config.mjs
+++ examples/remote/react-server.production.config.mjs
@@ -1,15 +1,15 @@
 export default {
   importMap: {
     imports: {
-      react: "https://esm.sh/react@0.0.0-experimental-9ff42a87-20250130",
+      react: "https://esm.sh/react@0.0.0-experimental-740a4f7a-20250325",
       "react/jsx-runtime":
-        "https://esm.sh/react@0.0.0-experimental-9ff42a87-20250130/jsx-runtime",
+        "https://esm.sh/react@0.0.0-experimental-740a4f7a-20250325/jsx-runtime",
       "react-dom":
-        "https://esm.sh/react-dom@0.0.0-experimental-9ff42a87-20250130",
+        "https://esm.sh/react-dom@0.0.0-experimental-740a4f7a-20250325",
       "react-dom/client":
-        "https://esm.sh/react-dom@0.0.0-experimental-9ff42a87-20250130/client",
+        "https://esm.sh/react-dom@0.0.0-experimental-740a4f7a-20250325/client",
       "react-server-dom-webpack/client.browser":
-        "https://esm.sh/react-server-dom-webpack@0.0.0-experimental-9ff42a87-20250130/client.browser",
+        "https://esm.sh/react-server-dom-webpack@0.0.0-experimental-740a4f7a-20250325/client.browser",
       "http://localhost:3003/client/__/__/packages/react-server/":
         "/client/__/__/packages/react-server/",
     },

4445f97447d411e4f849b90e1348a2c0016dc6ed
Viktor Lázár <lazarv1982@gmail.com>
2025-03-25 19:19:41 +0100

    feat: upgrade dependencies

--- examples/remote/react-server.development.config.mjs
+++ examples/remote/react-server.development.config.mjs
@@ -1,17 +1,17 @@
 export default {
   importMap: {
     imports: {
-      react: "https://esm.sh/react@0.0.0-experimental-9ff42a87-20250130?dev",
+      react: "https://esm.sh/react@0.0.0-experimental-740a4f7a-20250325?dev",
       "react/jsx-dev-runtime":
-        "https://esm.sh/react@0.0.0-experimental-9ff42a87-20250130/jsx-dev-runtime?dev",
+        "https://esm.sh/react@0.0.0-experimental-740a4f7a-20250325/jsx-dev-runtime?dev",
       "react/jsx-runtime":
-        "https://esm.sh/react@0.0.0-experimental-9ff42a87-20250130/jsx-runtime?dev",
+        "https://esm.sh/react@0.0.0-experimental-740a4f7a-20250325/jsx-runtime?dev",
       "react-dom":
-        "https://esm.sh/react-dom@0.0.0-experimental-9ff42a87-20250130?dev",
+        "https://esm.sh/react-dom@0.0.0-experimental-740a4f7a-20250325?dev",
       "react-dom/client":
-        "https://esm.sh/react-dom@0.0.0-experimental-9ff42a87-20250130/client?dev",
+        "https://esm.sh/react-dom@0.0.0-experimental-740a4f7a-20250325/client?dev",
       "react-server-dom-webpack/client.browser":
-        "https://esm.sh/react-server-dom-webpack@0.0.0-experimental-9ff42a87-20250130/client.browser?dev",
+        "https://esm.sh/react-server-dom-webpack@0.0.0-experimental-740a4f7a-20250325/client.browser?dev",
       "http://[::1]:3001/": "/",
       "http://localhost:3003/": "/",
     },

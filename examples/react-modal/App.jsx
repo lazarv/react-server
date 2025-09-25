3a6941e30501bbe9748694a1f4c79042a09335ca
Viktor Lázár <lazarv1982@gmail.com>
2025-09-25 23:35:13 +0200

    fix: externalize commonjs

---
+++ examples/react-modal/App.jsx
@@ -0,0 +1,11 @@
+import ModalParent from "./ModalParent.jsx";
+
+export default function App() {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <ModalParent />
+      </body>
+    </html>
+  );
+}

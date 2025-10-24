a61114d565a8eb2410c9831c1f357be47896a9ac
Viktor Lázár <lazarv1982@gmail.com>
2025-10-24 11:13:21 +0200

    fix: file-router test

--- test/__test__/apps/file-router.spec.mjs
+++ test/__test__/apps/file-router.spec.mjs
@@ -7,11 +7,14 @@ process.chdir(join(process.cwd(), "../examples/file-router"));
 
 test("file-router plugin", async () => {
   await server(null);
+
   await page.goto(`${hostname}/forms`);
+  await page.waitForLoadState("networkidle");
   expect(await page.textContent("body")).toContain("Layout (forms)");
   expect(await page.textContent("body")).not.toContain("Layout (forms simple)");
 
   await page.goto(`${hostname}/forms-simple`);
+  await page.waitForLoadState("networkidle");
   expect(await page.textContent("body")).not.toContain("Layout (forms)");
   expect(await page.textContent("body")).toContain("Layout (forms simple)");
 });

bc239d7ec7c561ac54adc12c5fe9d6cf31a64783
Viktor Lázár <lazarv1982@gmail.com>
2025-10-24 11:00:39 +0200

    fix: file router layout selection

---
+++ examples/file-router/pages/forms-simple/index.tsx
@@ -0,0 +1,8 @@
+export default function FormIndexPage() {
+  return (
+    <div>
+      <h1>Forms (simple) Index Page</h1>
+      <p>This is the index page for forms in the file-based routing example.</p>
+    </div>
+  );
+}
---
+++ test/__test__/apps/file-router.spec.mjs
@@ -0,0 +1,17 @@
+import { join } from "node:path";
+
+import { hostname, page, server } from "playground/utils";
+import { expect, test } from "vitest";
+
+process.chdir(join(process.cwd(), "../examples/file-router"));
+
+test("file-router plugin", async () => {
+  await server(null);
+  await page.goto(`${hostname}/forms`);
+  expect(await page.textContent("body")).toContain("Layout (forms)");
+  expect(await page.textContent("body")).not.toContain("Layout (forms simple)");
+
+  await page.goto(`${hostname}/forms-simple`);
+  expect(await page.textContent("body")).not.toContain("Layout (forms)");
+  expect(await page.textContent("body")).toContain("Layout (forms simple)");
+});

d4faffc8b9c06a9284daf008716631eb81abbe65
Viktor Lázár <lazarv1982@gmail.com>
2026-03-08 12:21:32 +0100

    fix: remote script

--- packages/react-server/bin/commands/dev.mjs
+++ packages/react-server/bin/commands/dev.mjs
@@ -10,10 +10,12 @@ export default (cli) =>
     })
     .option("--port <port>", "[number] port to listen on", { default: 3000 })
     .option("--https", "[boolean] use HTTPS protocol", { default: false })
+    .option("--cors", "enable CORS", { default: false })
+    .option("--origin <origin>", "[string] origin", { default: "" })
+    .option("--trust-proxy", "[boolean] trust proxy", { default: false })
     .option("--open [url]", "[boolean|string] open browser on server start", {
       default: false,
     })
-    .option("--cors", "enable CORS", { default: false })
     .option("--force", "force optimize deps", { default: false })
     .option("--watch", "watch for config changes", { default: true })
     .option("--clear-screen", "clear screen on server start", {
